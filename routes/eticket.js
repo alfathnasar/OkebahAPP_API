faa8e8304790abd7e67b9acfaee3f7c12a4ddbbf
alfathnasar <alfathnasar@gmail.com>
2023-09-26 01:20:45 +0800

    GET ETICKET

--- routes/eticket.js
+++ routes/eticket.js
@@ -2,7 +2,7 @@
     const eticketController = require('../controller/eticket.js');
     const router = express.Router();
 
-    router.get('/:username/:id_pemesanan', eticketController.getDataPenumpang);
+    //router.get('/:username/:id_pemesanan', eticketController.getDataPenumpang);
     router.get('/:username/:jenis_transportasi', eticketController.getETicket);
     router.post('/', eticketController.updatePurchasedStatus);
 
