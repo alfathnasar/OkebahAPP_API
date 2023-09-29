0176dceedba00a18b521db99b926f98a22f4a4e2
alfathnasar <alfathnasar@gmail.com>
2023-09-29 13:52:08 +0800

    FIX GET PURCHASED HISTORY

--- routes/purchased.js
+++ routes/purchased.js
@@ -3,7 +3,7 @@
     const router = express.Router();
 
     router.post('/', purchasedController.setNewPurchased);
-    router.get('/:username/:transportasi', purchasedController.getPurchased);
+    // router.get('/:username/:transportasi', purchasedController.getPurchased);
     router.get('/all/:username', purchasedController.getAllPurchased);
 
     module.exports = router;
