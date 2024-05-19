0215c60a164b0e04dbe3ad7f420c91f5b00dc69f
alfathnasar <alfathnasar@email.com>
2024-05-19 09:54:12 +0800

    CHECK IN

--- routes/eticket.js
+++ routes/eticket.js
@@ -3,7 +3,7 @@
     const router = express.Router();
 
     router.get('/:username/:id_pemesanan', eticketController.getDataPenumpang);
-    router.get('/:kode_speed/:id_destinasi', eticketController.getDataPenumpangAgen);
+    router.get('/checkin/:kode_speed/:id_destinasi', eticketController.getDataPenumpangAgen);
     router.get('/booking/:username/:jenis_transportasi', eticketController.getETicket);
     router.post('/', eticketController.updatePurchasedStatus);
     router.patch('/checkin/:kode_booking', eticketController.updateStatusEticket);
